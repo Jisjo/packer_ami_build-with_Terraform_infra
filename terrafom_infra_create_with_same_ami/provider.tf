bbef182a8a42fe4bab102404814159eb57d2551f
Jisjo K John <61861646+Jisjo@users.noreply.github.com>
2021-12-07 14:23:05 +0530

    v2

--- terrafom_infra_create_with_same_ami/provider.tf
+++ terrafom_infra_create_with_same_ami/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAVOYUBUYWMEBZCDXI"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
